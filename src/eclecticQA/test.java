41b6c9db0d5f992f1656b8e519fe62c1a583d1d6
Umut Balkan <umutbalkan@icloud.com>
2017-10-26 09:14:12 +0300

    IO
    grids and cells gets written to text file

--- src/eclecticQA/test.java
+++ src/eclecticQA/test.java
@@ -20,30 +20,52 @@ public class test {
 		 * - Finding Elements
 		 * - Getting Element data
 		 * */
-		
+		String yeah = "";
 		// Display title
 		String title = html_doc.title();
-		System.out.println(title);
+		//System.out.println(title);
+		yeah += title;
 		
 		// Get Clues
 		Elements clues = html_doc.getElementsByClass("Clue-li--1JoPu"); // ACROSS CLUES
 		//Element down_clues = html_doc.getElementsByClass("ClueList-list--2dD5-").last(); // DOWN CLUES
 		
-		System.out.println("\nACROSS");
+		yeah += "\nACROSS\n";
+		//System.out.println("\nACROSS");
 		for(int i = 0; i < clues.size(); i++) {
 			Element temp = clues.get(i);
-			if( i == 5) System.out.println("DOWN");
-			System.out.println(temp.child(0).text() + ") " + temp.child(1).text());
+			if( i == 5)   yeah += "DOWN\n";
+			//System.out.println(temp.child(0).text() + ") " + temp.child(1).text());
+			yeah += temp.child(0).text() + ") " + temp.child(1).text() + "\n";
 		}
 		
+		System.out.println(yeah);
 		
-		/*
+		// Fetch & Display grid and cells
+		Elements grids = html_doc.select("g[data-group]");
+		Elements cells = grids.first().select("rect");
+		String board = "";
+		for( int i = 0; i < cells.size(); i++) {
+			if(i % 5 == 0) board += "\n";
+			
+			Element t = cells.get(i);
+			String cell_type = t.className();
+			if (cell_type.equals("Cell-block--1oNaD")) board += "X ";
+			else if (cell_type.equals("Cell-cell--1p4gH")) board += "0 ";
+			
+		}
+		
+		System.out.println(board);
+		
+		
+		
 		// WRITE TXT FILE
 		BufferedWriter writer = null;
 		try
 		{
 		    writer = new BufferedWriter( new FileWriter(title + ".txt"));
-		    writer.write(clues.text());
+		    writer.write(yeah);
+		    writer.write(board);
 
 		}
 		catch ( IOException e) {}
@@ -56,35 +78,7 @@ public class test {
 		    }
 		    catch ( IOException e) {}
 		}
-		*/
 		
-		Elements grids = html_doc.select("g[data-group]");
-		Elements temp = grids.first().select("rect"); // gets data-group="cells"
-		//System.out.println(temp);
-		
-		for( int i = 0; i < temp.size(); i++) {
-			if(i % 5 == 0) System.out.println();
-			
-			Element t = temp.get(i);
-			String cell_type = t.className();
-			if(cell_type.equals("Cell-block--1oNaD")) System.out.print("X");
-			else if(cell_type.equals("Cell-cell--1p4gH")) System.out.print("0");
-			
-		}
-		
-		// Combining ACROSS & DOWN
-		//String across_txt = across_clues.text();
-		//String down_txt = down_clues.text();
-		//String parse = "ACROSS" + across_txt + "\nDOWN" + down_txt;
-		
-		// print
-		//for(int i = 0; i < parse.length(); i++) {
-		//	char tmp = parse.charAt(i);
-		//	if(tmp <= 57 && tmp >  48) // char is an integer
-		//		System.out.print("\n" + parse.charAt(i) + " - "); // print
-		//	else
-		//		System.out.print(parse.charAt(i)); // concatenate & print
-		//}//END_LOOP
 		
 
 	}//END_main()
